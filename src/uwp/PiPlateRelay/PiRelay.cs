e6a18bb3093961be66e8562007e7b12423a8ae20
Travis Whidden <travis.whidden@econnect.tv>
2017-12-15 21:11:41 -0800

    Fixed issues reading from SPI / Timing with SPI. Corrected Func names to not include Async because no special delays/timing had to be added.

--- src/uwp/PiPlateRelay/PiRelay.cs
+++ src/uwp/PiPlateRelay/PiRelay.cs
@@ -50,13 +50,13 @@ namespace PiPlateRelay
 
         private static SpiDevice _spi;
 
-        public static Task Inititlize()
+        public static async Task InititlizeAsync()
         {
             RelaysAvailable = Relays;
 
             InitGpio();
-            InitSpi();
-            return InitRelaysAsync();
+            await InitSpiAsync();
+            InitRelays();
         }
 
         /// <summary>
@@ -86,7 +86,7 @@ namespace PiPlateRelay
         /// <summary>
         /// Initilize the Spi per the board specifiction
         /// </summary>
-        private static void InitSpi()
+        private static async Task InitSpiAsync()
         {
             if (_spi == null)
             {
@@ -97,16 +97,18 @@ namespace PiPlateRelay
                     SharingMode = SpiSharingMode.Shared
                 };
                 var settings = spiSettings;
-                var deviceInformationCollection = DeviceInformation.FindAllAsync(SpiDevice.GetDeviceSelector("SPI0")).AsTask<DeviceInformationCollection>().ConfigureAwait(false).GetAwaiter().GetResult();
+                var deviceInformationCollection = await DeviceInformation.FindAllAsync(SpiDevice.GetDeviceSelector("SPI0"));
                 if (deviceInformationCollection.Count == 0)
                 {
                     throw new PiRelayException("There is no SPI bus 0 on this device.");
                 }
+
                 _spi = SpiDevice.FromIdAsync(deviceInformationCollection[0].Id, settings).AsTask<SpiDevice>().ConfigureAwait(false).GetAwaiter().GetResult();
                 if (_spi == null)
                 {
                     throw new PiRelayException("There is no SPI bus 0 on this device.");
                 }
+                
             }
         }
 
@@ -114,30 +116,39 @@ namespace PiPlateRelay
         /// Detect the Relay boards. There could up to 8 boards together
         /// </summary>
         /// <returns></returns>
-        private static async Task InitRelaysAsync()
+        private static void InitRelays()
         {
-            for (byte possibleAddress = 0; possibleAddress < 8; possibleAddress++)
+            // For some reason, the first command needs to be sent over the SPI
+            // There most likely is something I can send after I init the SPI
+            // but this is just a simple hack that works.
+            SendCommand(0, PiRelayCommand.SystemAddress, 0, 0, 1, true);
+
+            for (byte address = 0; address < 8; address++)
             {
-                var cmd = await SendCommandAsync(possibleAddress, PiRelayCommand.SystemAddress, 0, 0, 1, true);
-                var returnedAddress = cmd[0];
-                if (returnedAddress != possibleAddress) continue;
+                var cmd = SendCommand(address, PiRelayCommand.SystemAddress, 0, 0, 1, true);
+                //cmd = await SendCommand(address, PiRelayCommand.SystemId, 0, 0, 20, true);
+                var returnedAddress = cmd[0] - GpioBaseAddr;
+                if (returnedAddress != address) continue;
+
+                var id = SendCommand(address, PiRelayCommand.SystemId, 0, 0, 20, true);
+                var idStr = Encoding.ASCII.GetString(id);
                 var state = new PiRelayInfo()
                 {
-                    FirmwareRevision = await GetFirmwreRevisionAsync(possibleAddress, true),
-                    HardwareRevision = await GetHardwareRevisionAsync(possibleAddress, true)
+                    FirmwareRevision = GetFirmwreRevision(address, true),
+                    HardwareRevision = GetHardwareRevision(address, true),
+                    HardwareId = idStr
                 };
                 // Reset the states to off
                 for(byte relay = 1; relay <= 7; relay++)
                 {
-                    await Task.Delay(TimeSpan.FromMilliseconds(1));
-                    await RelayOffAsync(returnedAddress, relay, true);
+                    RelayOffAsync(address, relay, true);
                 }
-                Relays.Add(possibleAddress, state);
+                Relays.Add(address, state);
             }
             _isInitilized = true;
         }
 
-        public static async Task<byte[]> SendCommandAsync(byte address, PiRelayCommand piRelayCommand, byte param1, byte param2, int bytes2Return, bool ignoreChecks = false)
+        public static byte[] SendCommand(byte address, PiRelayCommand piRelayCommand, byte param1, byte param2, int bytes2Return, bool ignoreChecks = false)
         {
             // Checks to ensure that the library has been initlized and the relay
             // address is available.
@@ -147,47 +158,58 @@ namespace PiPlateRelay
                 if (!Relays.ContainsKey(address)) throw new PiRelayException($"PiRelay Address {address} not available");
             }
 
+            var addr = address + GpioBaseAddr;
+
             var arg = new byte[4];
             var resp = new byte[bytes2Return];
-            arg[0] = (byte)(address + GpioBaseAddr);
+            arg[0] = (byte)(addr);
             arg[1] = (byte)piRelayCommand;
             arg[2] = param1;
             arg[3] = param2;
+
             _ppFrame.Write(GpioPinValue.High);
+            _spi.ConnectionSettings.DataBitLength = 60;
+            _spi.ConnectionSettings.ClockFrequency = 300000;
             _spi.Write(arg);
+
             if (bytes2Return > 0)
             {
-                await Task.Delay(TimeSpan.FromSeconds(0.0001));
                 _spi.ConnectionSettings.ClockFrequency = 500000;
+                _spi.ConnectionSettings.DataBitLength = 20;
                 for (var i = 0; i < bytes2Return; i++)
                 {
-                    var dummy = new byte[1];
-                    _spi.TransferFullDuplex(new byte[1], dummy);
-                    resp[i] = dummy[0];
+                    byte[] r = new byte[1];
+                    _spi.TransferFullDuplex(r, r);
+                    resp[i] = r[0];
                 }
             }
+
             _ppFrame.Write(GpioPinValue.Low);
+
+
             return resp;
         }
 
-        public static async Task<string> GetIdAsync(byte address, bool ignoreChecks)
+        
+
+        public static string GetId(byte address, bool ignoreChecks)
         {
-            var result = await SendCommandAsync(address, PiRelayCommand.SystemId, 0, 0, 20, ignoreChecks);
+            var result = SendCommand(address, PiRelayCommand.SystemId, 0, 0, 20, ignoreChecks);
             return Encoding.ASCII.GetString(result);
         }
 
-        public static async Task<double> GetHardwareRevisionAsync(byte address, bool ignoreChecks = false)
+        public static double GetHardwareRevision(byte address, bool ignoreChecks = false)
         {
-            var resp = await SendCommandAsync(address, PiRelayCommand.SystemHardwareRevision, 0, 0, 1, ignoreChecks);
+            var resp = SendCommand(address, PiRelayCommand.SystemHardwareRevision, 0, 0, 1, ignoreChecks);
             var rev = resp[0];
             var whole = rev >> 4;
             var point = rev & 0x0F;
             return whole + point / 10.0;
         }
 
-        public static async Task<double> GetFirmwreRevisionAsync(byte address, bool ignoreChecks = false)
+        public static double GetFirmwreRevision(byte address, bool ignoreChecks = false)
         {
-            var resp = await SendCommandAsync(address, PiRelayCommand.SystemHardwareRevision, 0, 0, 1, ignoreChecks);
+            var resp = SendCommand(address, PiRelayCommand.SystemHardwareRevision, 0, 0, 1, ignoreChecks);
             var rev = resp[0];
             var whole = rev >> 4;
             var point = rev & 0x0F;
@@ -197,56 +219,56 @@ namespace PiPlateRelay
         //#==============================================================================#	
         //# LED Functions	                                                               #
         //#==============================================================================#   
-        public static  Task LedSetAsync(byte address, bool ignoreChecks = false)
+        public static void LedSet(byte address, bool ignoreChecks = false)
         {
-            return SendCommandAsync(address, PiRelayCommand.LedSet, 0, 0, 0, ignoreChecks);
+            SendCommand(address, PiRelayCommand.LedSet, 0, 0, 0, ignoreChecks);
         }
 
-        public static Task LedClearAsync(byte address, bool ignoreChecks = false)
+        public static void LedClearAsync(byte address, bool ignoreChecks = false)
         {
-            return SendCommandAsync(address, PiRelayCommand.LedClear, 0, 0, 0, ignoreChecks);
+            SendCommand(address, PiRelayCommand.LedClear, 0, 0, 0, ignoreChecks);
         }
 
-        public static Task ToggleLedAsync(byte address, bool ignoreChecks = false)
+        public static void ToggleLedAsync(byte address, bool ignoreChecks = false)
         {
-            return SendCommandAsync(address, PiRelayCommand.LedToggle, 0, 0, 0, ignoreChecks);
+            SendCommand(address, PiRelayCommand.LedToggle, 0, 0, 0, ignoreChecks);
         }
 
         //#==============================================================================#	
         //# PiRelay Functions	                                                               #
         //#==============================================================================#   
 
-        public static Task RelayOnAsync(byte address, byte relay, bool ignoreChecks = false)
+        public static void RelayOnAsync(byte address, byte relay, bool ignoreChecks = false)
         {
-            return SendCommandAsync(address, PiRelayCommand.RelayOn, relay, 0, 0, ignoreChecks);
+            SendCommand(address, PiRelayCommand.RelayOn, relay, 0, 0, ignoreChecks);
         }
 
-        public static Task RelayOffAsync(byte address, byte relay, bool ignoreChecks = false)
+        public static void RelayOffAsync(byte address, byte relay, bool ignoreChecks = false)
         {
-            return SendCommandAsync(address, PiRelayCommand.RelayOff, relay, 0, 0, ignoreChecks);
+            SendCommand(address, PiRelayCommand.RelayOff, relay, 0, 0, ignoreChecks);
         }
 
-        public static Task RelayToggleAsync(byte address, byte relay, bool ignoreChecks = false)
+        public static void RelayToggleAsync(byte address, byte relay, bool ignoreChecks = false)
         {
-            return SendCommandAsync(address, PiRelayCommand.RelayToggle, relay, 0, 0, ignoreChecks);
+            SendCommand(address, PiRelayCommand.RelayToggle, relay, 0, 0, ignoreChecks);
         }
 
-        public static Task RelayToggleAsync(byte relay, bool ignoreChecks = false)
+        public static void RelayToggleAsync(byte relay, bool ignoreChecks = false)
         {
             var r = GetAddressRelay(relay);
-            return SendCommandAsync(r.Item1, PiRelayCommand.RelayToggle, r.Item2, 0, 0, ignoreChecks);
+            SendCommand(r.Item1, PiRelayCommand.RelayToggle, r.Item2, 0, 0, ignoreChecks);
         }
 
-        public static Task RelayOnAsync(byte relay, bool ignoreChecks = false)
+        public static void RelayOnAsync(byte relay, bool ignoreChecks = false)
         {
             var r = GetAddressRelay(relay);
-            return SendCommandAsync(r.Item1, PiRelayCommand.RelayOn, r.Item2, 0, 0, ignoreChecks);
+            SendCommand(r.Item1, PiRelayCommand.RelayOn, r.Item2, 0, 0, ignoreChecks);
         }
 
-        public static Task RelayOffAsync(byte relay, bool ignoreChecks = false)
+        public static void RelayOffAsync(byte relay, bool ignoreChecks = false)
         {
             var r = GetAddressRelay(relay);
-            return SendCommandAsync(r.Item1, PiRelayCommand.RelayOff, r.Item2, 0, 0, ignoreChecks);
+            SendCommand(r.Item1, PiRelayCommand.RelayOff, r.Item2, 0, 0, ignoreChecks);
         }
 
         internal static Tuple<byte, byte> GetAddressRelay(byte relay)
@@ -256,16 +278,16 @@ namespace PiPlateRelay
             return new Tuple<byte, byte>(address, deviceRelay);
         }
         
-        public static Task RelayAllAsync(byte address, byte relays, bool ignoreChecks = false)
+        public static void RelayAllAsync(byte address, byte relays, bool ignoreChecks = false)
         {
             if (relays > 127) throw new PiRelayException("Argument out of range. Must be between 0 and 127");
 
-            return SendCommandAsync(address, PiRelayCommand.RelayAll, relays, 0, 0, ignoreChecks);
+            SendCommand(address, PiRelayCommand.RelayAll, relays, 0, 0, ignoreChecks);
         }
 
-        public static async Task<byte> RelayStateAsync(byte address, bool ignoreChecks = false)
+        public static byte RelayStateAsync(byte address, bool ignoreChecks = false)
         {
-            var r = await SendCommandAsync(address, PiRelayCommand.RelayState, 0, 0, 1, ignoreChecks);
+            var r = SendCommand(address, PiRelayCommand.RelayState, 0, 0, 1, ignoreChecks);
             return r[0];
         }
     }
