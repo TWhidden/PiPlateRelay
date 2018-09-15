30ae4f86a8b8696504b9d130f88417c521046438
Travis Whidden <travis@lvfbody.com>
2018-09-15 14:56:44 -0700

    PiRelayPlate.NetCore Test app with embeded resource added

--- src/netcore/PiRelayPlate.NetCore.TestConsole/Program.cs
+++ src/netcore/PiRelayPlate.NetCore.TestConsole/Program.cs
@@ -6,7 +6,26 @@ namespace PiRelayPlate.NetCore.TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Loop over the relay stack
+            var devicesToProcess = 7;
+            var pinsperDevice = 7;
+
+            for (byte devicesId = 0; devicesId < devicesToProcess; devicesId++)
+            {
+                for (byte pin = 0; pin < pinsperDevice; pin++)
+                {
+                    Console.WriteLine($"Device: {devicesId}; Pin: {pin}; (cycle)");
+                    var result = RelayPlate.SetPinState(devicesId, pin, 1);
+                    if (result != 0)
+                    {
+                        Console.WriteLine($"Device: {devicesId}; Pin: {pin} failed with returned with code {result}");
+                        continue;
+                    }
+                    System.Threading.Thread.Sleep(250);
+                    RelayPlate.SetPinState(devicesId, pin, 0);
+                }
+            }
+
         }
     }
 }
--- src/netcore/PiRelayPlate.NetCore/RelayPlate.cs
+++ src/netcore/PiRelayPlate.NetCore/RelayPlate.cs
@@ -1,13 +1,49 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
 
-namespace RelayPlateCore
+namespace PiRelayPlate.NetCore
 {
     public static class RelayPlate
     {
+        private const string LibFileName = "libRelayPlate.so";
+
+        static RelayPlate()
+        {
+            // Ensure the file has been created / updated in the directory
+            var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var libPath = Path.Combine(executingDirectory, LibFileName);
+
+            if (File.Exists(libPath))
+            {
+                // attempt to remove the file first
+                try
+                {
+                    File.Delete(libPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Could not remove file {LibFileName}. Error: {ex.Message}");
+                }
+            }
+
+            // Attempt to write the current resource file
+            try
+            {
+                File.WriteAllBytes(libPath, Properties.Resources.libRelayPlate);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not add file {LibFileName}. Error: {ex.Message}");
+            }
+        }
+
+
         // ToDo: fix mangled name on c export
-        [DllImport("libblinkLib.so", EntryPoint = "_Z11SetPinStatehhh", CallingConvention = CallingConvention.StdCall, PreserveSig = true), SuppressUnmanagedCodeSecurity]
+        [DllImport(LibFileName, EntryPoint = "_Z11SetPinStatehhh", CallingConvention = CallingConvention.StdCall, PreserveSig = true), SuppressUnmanagedCodeSecurity]
         public static extern int SetPinState(byte boardId, byte pin, byte state);
     }
 }
