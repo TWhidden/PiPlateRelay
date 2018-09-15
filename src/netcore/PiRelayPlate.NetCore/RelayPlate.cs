47895bfeeac4988bd263a3ab915cf0204ed057a7
Travis Whidden <travis@lvfbody.com>
2018-09-15 14:31:01 -0700

    RelayPlate .net Core 2.1 impl - using c++ libs

---
+++ src/netcore/PiRelayPlate.NetCore/RelayPlate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace RelayPlateCore
+{
+    public static class RelayPlate
+    {
+        // ToDo: fix mangled name on c export
+        [DllImport("libblinkLib.so", EntryPoint = "_Z11SetPinStatehhh", CallingConvention = CallingConvention.StdCall, PreserveSig = true), SuppressUnmanagedCodeSecurity]
+        public static extern int SetPinState(byte boardId, byte pin, byte state);
+    }
+}
